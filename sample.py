71b9684120cfea42533b10abe80d475cf021f43a
Ren <reneto523@gmail.com>
2019-06-04 16:55:47 +0900

    list creation
    created and manipulated new lists for practice

--- sample.py
+++ sample.py
@@ -1,3 +1,36 @@
-print("Hello")
+list1 = [0, 1, 2]
+list2 = [3, 4, 5]
+list3 = ["Yuko", "Ren", "Yushi", "Esteban"]
+list4 = [0, 1, 2, 3, 4, 5, 6, 7]
 
-print("My name is Ren")
+
+total = list1 + list2
+total_new = []
+total_name = []
+total_esteban = []
+
+for i in total:
+    num = i + 3
+    total_new.append(num)
+
+for i in list3:
+    name = i + "-san"
+    total_name.append(name)
+
+for i in list3:
+    if i == "Esteban":
+        new_name = i + "-san"
+    else:
+        new_name = i
+    total_esteban.append(new_name)
+    
+
+#Homework by 6/7 Friday
+
+#Pick up names if you find an 'n' inside the name and create new list and print
+#Divide the list4 into even and odd lists
+
+print(total)
+print(total_new)
+print(total_name)
+print(total_esteban)
